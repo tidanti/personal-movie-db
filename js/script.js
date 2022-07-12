d9325d3340eaace0dd92097a374403483040759f
tidanti <tipanovdaniil@gmail.com>
2022-07-12 21:23:01 +0300

    added personal movie object

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+// 1
 const numberOfFilms = +prompt('Number of watched films?', '0');
+
+// 2
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
