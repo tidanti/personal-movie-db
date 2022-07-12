e5f9159fd4334643623d3bd8943f59c7d0df9504
tidanti <tipanovdaniil@gmail.com>
2022-07-12 21:49:21 +0300

    added getting and writing user info about films to object, updated html

--- js/script.js
+++ js/script.js
@@ -3,9 +3,17 @@ const numberOfFilms = +prompt('Number of watched films?', '0');
 
 // 2
 const personalMovieDB = {
-    count: 0,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
+
+// 3
+for (let i = 0; i < 2; i++) {
+    const lastWatchedFilm = prompt('Last watched film?', '');
+    const userScoreOfLastWatchedFilm = prompt('Your score?', '8.1');
+
+    personalMovieDB.movies[lastWatchedFilm] = userScoreOfLastWatchedFilm;
+}
